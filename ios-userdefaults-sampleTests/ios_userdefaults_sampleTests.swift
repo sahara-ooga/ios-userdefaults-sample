f119cd0aa2b11b11833a393bcb56717df2b2e5d1
yuu ogasawara <performa532@gmail.com>
2018-06-16 10:57:41 +0900

    Initial Commit

---
+++ ios-userdefaults-sampleTests/ios_userdefaults_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_userdefaults_sampleTests.swift
+//  ios-userdefaults-sampleTests
+//
+//  Created by ogasawara_dev on 2018/06/16.
+//  Copyright © 2018年 ogasawara_dev. All rights reserved.
+//
+
+import XCTest
+@testable import ios_userdefaults_sample
+
+class ios_userdefaults_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
